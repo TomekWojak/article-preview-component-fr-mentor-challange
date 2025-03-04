04bcc4487770f7a3ecdf4cbd918adcffe0f09d7d
Tomek <tomek.wojak796@gmail.com>
2025-03-04 12:24:35 +0100

    finish mobile layout

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+const shareBtn = document.querySelector('.cards__card-link')
+const socialBox = document.querySelector('.cards__card-social-box')
+
+shareBtn.addEventListener('click', () => {
+    socialBox.classList.toggle('active')
+})
