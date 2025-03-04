e2745418a58de6c64cc0878b081b29142b072458
Tomek <tomek.wojak796@gmail.com>
2025-03-04 13:39:28 +0100

    finished site

--- js/app.js
+++ js/app.js
@@ -4,4 +4,5 @@ const socialBox = document.querySelector('.cards__card-social-box')
 
 shareBtn.addEventListener('click', () => {
     socialBox.classList.toggle('active')
+    shareBtn.classList.toggle('active')
 })
